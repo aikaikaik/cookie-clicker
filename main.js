ac9ee8dabc8205aa39c1b298ed65144c50fc8934
arad keren <aradk1@gmail.com>
2015-09-10 14:04:24 +0300

    your state bagelified

--- main.js
+++ main.js
@@ -1786,32 +1786,32 @@ Game.Launch=function()
 
 			if (list.length==0)
 			{
-				if (Game.cookiesEarned<5) list.push('You feel like making cookies. But nobody wants to eat your cookies.');
-				else if (Game.cookiesEarned<50) list.push('Your first batch goes to the trash. The neighborhood raccoon barely touches it.');
-				else if (Game.cookiesEarned<100) list.push('Your family accepts to try some of your cookies.');
-				else if (Game.cookiesEarned<500) list.push('Your cookies are popular in the neighborhood.');
-				else if (Game.cookiesEarned<1000) list.push('People are starting to talk about your cookies.');
-				else if (Game.cookiesEarned<3000) list.push('Your cookies are talked about for miles around.');
-				else if (Game.cookiesEarned<6000) list.push('Your cookies are renowned in the whole town!');
-				else if (Game.cookiesEarned<10000) list.push('Your cookies bring all the boys to the yard.');
-				else if (Game.cookiesEarned<20000) list.push('Your cookies now have their own website!');
-				else if (Game.cookiesEarned<30000) list.push('Your cookies are worth a lot of money.');
-				else if (Game.cookiesEarned<40000) list.push('Your cookies sell very well in distant countries.');
-				else if (Game.cookiesEarned<60000) list.push('People come from very far away to get a taste of your cookies.');
-				else if (Game.cookiesEarned<80000) list.push('Kings and queens from all over the world are enjoying your cookies.');
-				else if (Game.cookiesEarned<100000) list.push('There are now museums dedicated to your cookies.');
-				else if (Game.cookiesEarned<200000) list.push('A national day has been created in honor of your cookies.');
+				if (Game.cookiesEarned<5) list.push('You feel like making bagels. But nobody wants to eat your bagels, yes even Uri(p).');
+				else if (Game.cookiesEarned<50) list.push('Your first batch goes to the trash. Uri(p) barely touches it.');
+				else if (Game.cookiesEarned<100) list.push('Your classmates accepts to try some of your bagels.');
+				else if (Game.cookiesEarned<500) list.push('Your bagels are popular in the gifted-o-shpere.');
+				else if (Game.cookiesEarned<1000) list.push('People are starting to talk about your bagels.');
+				else if (Game.cookiesEarned<3000) list.push('Your bagels are talked about for megameters around.');
+				else if (Game.cookiesEarned<6000) list.push('Your bagels are renowned in the whole Te-Aviv district!');
+				else if (Game.cookiesEarned<10000) list.push('Your bagels bring all the boys to the yard, a thing which is of couse, impossible.');
+				else if (Game.cookiesEarned<20000) list.push('Your bagels now have their own website and domain!');
+				else if (Game.cookiesEarned<30000) list.push('Your bagels are worth a lot of money.');
+				else if (Game.cookiesEarned<40000) list.push('Your bagels sell very well in distant countries.');
+				else if (Game.cookiesEarned<60000) list.push('People come from very far away to get a taste of your bagels.');
+				else if (Game.cookiesEarned<80000) list.push('Kings and queens from all over the world are enjoying your bagels.');
+				else if (Game.cookiesEarned<100000) list.push('There are now museums and archives dedicated to your bagels.');
+				else if (Game.cookiesEarned<200000) list.push('A national day has been created in honor of your bagels.');
 				else if (Game.cookiesEarned<300000) list.push('Your cookies have been named a part of the world wonders.');
 				else if (Game.cookiesEarned<450000) list.push('History books now include a whole chapter about your cookies.');
-				else if (Game.cookiesEarned<600000) list.push('Your cookies have been placed under government surveillance.');
-				else if (Game.cookiesEarned<1000000) list.push('The whole planet is enjoying your cookies!');
-				else if (Game.cookiesEarned<5000000) list.push('Strange creatures from neighboring planets wish to try your cookies.');
-				else if (Game.cookiesEarned<10000000) list.push('Elder gods from the whole cosmos have awoken to taste your cookies.');
-				else if (Game.cookiesEarned<30000000) list.push('Beings from other dimensions lapse into existence just to get a taste of your cookies.');
-				else if (Game.cookiesEarned<100000000) list.push('Your cookies have achieved sentience.');
-				else if (Game.cookiesEarned<300000000) list.push('The universe has now turned into cookie dough, to the molecular level.');
-				else if (Game.cookiesEarned<1000000000) list.push('Your cookies are rewriting the fundamental laws of the universe.');
-				else if (Game.cookiesEarned<10000000000) list.push('A local news station runs a 10-minute segment about your cookies. Success!<br><span style="font-size:50%;">(you win a cookie)</span>');
+				else if (Game.cookiesEarned<600000) list.push('Your bagels have been placed under government surveillance.');
+				else if (Game.cookiesEarned<1000000) list.push('The whole planet is enjoying your bagels! It is very sad.');
+				else if (Game.cookiesEarned<5000000) list.push('Strange creatures from neighboring planets wish to try your bagels.');
+				else if (Game.cookiesEarned<10000000) list.push('Elder gods from the whole cosmos have awoken to taste your bagels.');
+				else if (Game.cookiesEarned<30000000) list.push('Beings from other dimensions lapse into existence just to get a taste of your bagels.');
+				else if (Game.cookiesEarned<100000000) list.push('Your bagels have achieved sentience, and now the world is a totalitarian communist dictatorship.');
+				else if (Game.cookiesEarned<300000000) list.push('The universe has now turned into bagel dough, to the molecular level.');
+				else if (Game.cookiesEarned<1000000000) list.push('Your bagels are rewriting the fundamental laws of the universe, now you\'ll need to relearn them.');
+				else if (Game.cookiesEarned<10000000000) list.push('A local news station runs a 10-minute segment about your bagels. Success!<br><span style="font-size:50%;">(you win a cookie)</span>');
 				else if (Game.cookiesEarned<10100000000) list.push('it\'s time to stop playing');//only show this for 100 millions (it's funny for a moment)
 			}
 
